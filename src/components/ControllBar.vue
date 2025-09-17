56b82d8c843a6494cb65b8953fa9f1590f039361
Yurii Bizhyk <qwantiix01@gmail.com>
2025-09-17 13:56:38 +0300

    miss

---
+++ src/components/ControllBar.vue
@@ -0,0 +1,15 @@
+
+<template>
+<!--
+  хай пояснить нахуя : в vue
+  і то як прцюють компоненти
+-->
+  <backdrop  variant="dark" :roundness="22" :height="96"></backdrop>
+</template>
+
+<style lang="css">
+
+</style>
+<script setup lang="ts">
+import Backdrop from "./ui/Backdrop.vue";
+</script>
